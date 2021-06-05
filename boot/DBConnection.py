4fc067d52cbf32eb6cc4472c89a771c4bbb5e38e
rjOwl <mostafayossef100@yahoo.com>
2021-06-05 03:22:08 +0200

    Used the connection string not the .env

--- boot/DBConnection.py
+++ boot/DBConnection.py
@@ -2,7 +2,8 @@ import mongoengine as db
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
-connectionString = "mongodb+srv://{}:{}@{}.mrby6.mongodb.net/{}?retryWrites=true&w=majority".format(config["MONGO_USERNAME"], config["MONGO_PASS"], config["MONGO_CLUSTER"], config["MONGO_DB_NAME"])
+# connectionString = "mongodb+srv://{}:{}@{}.mrby6.mongodb.net/{}?retryWrites=true&w=majority".format(config["MONGO_USERNAME"], config["MONGO_PASS"], config["MONGO_CLUSTER"], config["MONGO_DB_NAME"])
+connectionString = "mongodb+srv://team7-ninjas:123intake41@goodbookscluster.mrby6.mongodb.net/shortenedURLSDB?retryWrites=true&w=majority"
 try:
     db.connect(host=connectionString)
 except Exception as e:
